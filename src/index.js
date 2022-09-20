19ca5273ea22c25552ff3de6d39442adaebc32ce
Shivam <shivamdhruvmishra4@gmail.com>
2022-09-20 21:23:33 +0530

    plugins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import HelloWorldButton from './Start.js' 
+import HelloWorldButton from './components/Button/start.js' 
+import Heading from './components/Heading/Heading.js'
 
+const heading = new Heading()
 const helloWorld = new HelloWorldButton()
 
 helloWorld.render();
+heading.render()
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,11 +1,16 @@
 var path = require('path')
+var TerserPlugin = require('terser-webpack-plugin')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+var { CleanWebpackPlugin } = require('clean-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
-        publicPath: 'dist/'
+        publicPath: ''
     },
     mode: 'none',
     module: {
@@ -16,12 +21,47 @@ module.exports = {
                     'file-loader' 
                 ]
             },
+            // Before styles where resolved using loader which inject styles in index.html but with plugin it will create style.css and import it in index.html
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         'style-loader', 'css-loader'
+            //     ]
+            // },
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader', 'css-loader'
+                    MiniCssExtractPlugin.loader, 'css-loader'
                 ]
+            },
+            // {
+            //     text: /\.(hbs | pug)$/,
+            //     use: [
+            //         'handlebars-loader'
+            //     ]
+            // },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/env' ],
+                    }
+                }
             }
         ]
-    }
+    },
+    plugins: [
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({ filename: 'styles.[contenthash].css'}),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Home',
+            filename: 'index.html',
+            // template: 'file path' use templating engine for template
+            description: 'Some Description'
+            
+        })
+    ]
 }
